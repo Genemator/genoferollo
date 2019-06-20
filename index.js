8ddbfb5f0137f1b0ba788b67dd02232173924496
Genemator <genemator@icloud.com>
2019-06-20 18:26:11 +0500

    Edition 3

--- index.js
+++ index.js
@@ -1,4 +1,12 @@
+// Adding ip dotenv
 require('./env');
+
+// Importing bot handler and tokens, everything
 const { bot } = require('./core/bot');
+
+// Actions and logics of the bot
 require('./actions')(bot);
+
+// Commands of the bot, there are not so many commands but they
+// are the beginning for the bot. Just a startup folder....
 require('./commands')(bot);
